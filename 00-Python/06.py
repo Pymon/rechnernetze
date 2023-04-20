028ee84389bb94a3ce02f9ca88696f3025de76f5
Pymon <simonguenther02@gmail.com>
2023-04-20 11:03:00 +0200

    Added structure for task 6

---
+++ 00-Python/06.py
@@ -0,0 +1,27 @@
+class QuizMaster:
+    def __init__(self):
+        pass
+
+    def add_question(question_text: str, answers: List[str], correct_answers: List[str]):
+        pass
+
+    def pick_question(self):
+        pass
+
+class Question:
+    def __init__(self, question_text: str, answers: List[str], correct_answers: List[str]):
+        self.question_text = question_text
+        self.answers = answers
+        self.correct_answers = correct_answers
+
+    def shuffle():
+        pass
+
+    def answer(selected_answers: List[int]) -> List[bool]:
+        pass
+
+def index_to_letter(index: int) -> str:
+    pass
+
+def letter_to_index(letter: str) -> int:
+    pass
