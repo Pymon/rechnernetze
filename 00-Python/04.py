cd88ac5e30c29a05cd8458d66d4f0e0f30bb4f0d
Pymon <simonguenther02@gmail.com>
2023-04-25 10:37:00 +0200

    Added whitespace after if

--- 00-Python/04.py
+++ 00-Python/04.py
@@ -1,6 +1,6 @@
 def parity_bit(binaryAsString):
     countOfOnes = binaryAsString.count("1")
-    if(countOfOnes % 2 != 0):
+    if (countOfOnes % 2 != 0):
         return "1" + binaryAsString
     else:
         return "0" + binaryAsString
